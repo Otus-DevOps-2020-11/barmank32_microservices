b0d8aac691b5183fb7915455fd7d24f5201363e2
Denis Palaguta <barmank@yandex.ru>
2021-03-05 23:16:28 +0300

    Alertmanager Fix

--- monitoring/alertmanager/docker-entrypoint.sh
+++ monitoring/alertmanager/docker-entrypoint.sh
@@ -3,9 +3,9 @@
 cat /etc/alertmanager/config.yml |\
     sed "s@#api_url: <url>#@api_url: '$SLACK_URL'@g" |\
     sed "s@#channel: <channel>#@channel: '#$SLACK_CHANNEL'@g" |\
-    sed "s@#username: <user>#@username: '$SLACK_USER'@g" > /tmp/config.yml
+    sed "s@#username: <user>#@username: '$SLACK_USER'@g" > /tmp/alertmanager.yml
 
-mv /tmp/config.yml /etc/alertmanager/config.yml
+mv /tmp/alertmanager.yml /alertmanager/alertmanager.yml
 
 set -- /bin/alertmanager "$@"
 
